510e560788dbd9653e4f3e51e0f9779a078366d3
jeffstahlnecker <jeff@stahlnecker.me>
2020-10-22 14:56:18 +0200

    added external gw locations

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 const chalk = require('chalk')
+const data = require('./globalLocations.json')
+
 const log = console.log
 
 exports.sourceNodes = async (
-    { actions: { createNode }, createContentDigest, createNodeId },
+    { actions: { createNode, createTypes }, createContentDigest, createNodeId },
     { supernode }
 ) => {
     log(chalk.black.bgWhite(`Getting the data from ${supernode}`))
@@ -16,9 +18,9 @@ exports.sourceNodes = async (
         return
     }
 
-  // Get the Gateway Location API using Axios
+    // Get the Gateway Location API using Axios
 
-    const eachGatewayLocation = supernode.map(supernode => {
+    const eachGatewayLocation = supernode.map((supernode) => {
         return axios.get(`${supernode}/api/gateways-loc`)
     })
 
@@ -32,23 +34,36 @@ exports.sourceNodes = async (
     // Create Nodes for Gateway Locations
 
     const result = getData.flat()
-    if (result) {
-        let count = 0
-        result.forEach((location) => {
-            count++
-            const nodeContent = JSON.stringify(location)
-            const nodeMeta = {
-                id: createNodeId(`mxcSupernode-${count}`),
-                parent: null,
-                children: [],
-                internal: {
-                    type: `mxcSupernode`,
-                    content: nodeContent,
-                    contentDigest: createContentDigest(location),
-                },
-            }
-            const node = Object.assign({}, location, nodeMeta)
-            createNode(node)
-        })
+
+    // Introduce external data file
+
+    for (let i = 0; i < data.length; i++) {
+        data[i].location.latitude = +data[i].location.latitude
+        data[i].location.longitude = +data[i].location.longitude
     }
+    function setNode(dataSet, type) {
+        if (dataSet) {
+            let count = 0
+            dataSet.forEach((location) => {
+                count++
+                const nodeContent = JSON.stringify(location)
+
+                const nodeMeta = {
+                    id: createNodeId(`${type}-${count}`),
+                    parent: null,
+                    children: [],
+                    internal: {
+                        type: type,
+                        content: nodeContent,
+                        contentDigest: createContentDigest(location),
+                    },
+                }
+                const node = Object.assign({}, location, nodeMeta)
+                createNode(node)
+            })
+        }
+    }
+
+    setNode(result, 'mxcSupernode')
+    setNode(data, 'lpwanGateways')
 }
